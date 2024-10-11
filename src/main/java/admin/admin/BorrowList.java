4a8826774e94e5e036dd0e059c5922c7300943bd
陈兴 <2938244975@qq.com>
2024-10-11 13:37:35 +0800

    更新数据库

--- src/main/java/admin/admin/BorrowList.java
+++ src/main/java/admin/admin/BorrowList.java
@@ -6,7 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import javax.servlet            .ServletException;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
